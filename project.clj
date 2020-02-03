97bd64704db83caa7cf5e2cb50c02985b116fb20
Rume Ungrimu <rume.ungrimu@protonmail.com>
2020-02-03 18:32:59 +1000

    [#3] Added CLI interface

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/test.check "0.9.0"]]
+                 [org.clojure/test.check "0.9.0"]
+                 [org.clojure/tools.cli "0.4.2"]]
   :main ^:skip-aot kirke.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/kirke/core.clj
+++ src/kirke/core.clj
@@ -1,8 +1,17 @@
 (ns kirke.core
-  (:require [kirke.configurer :as cfg])
+  (:require [kirke.configurer :as cfg]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def cli-options
+  ;; An option with a required argument
+  [["-c" "--config CONFIG_FILE" "Config file"]
+   ["-v" "--verbose" "Verbose" :id :verbosity]
+   ["-h" "--help" "Print help message"]])
+
+(defn -main [& args]
+  (let [{:keys [options errors summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options) (do (println summary) (System/exit 0))
+      errors (do (println errors) (System/exit -1))
+      :else (println (slurp (:config options))))))
