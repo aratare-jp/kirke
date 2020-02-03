b527c5e255c1f1b01c569b81c1ed30870d2b7cf3
Rume Ungrimu <rume.ungrimu@protonmail.com>
2020-02-03 17:38:57 +1000

    [#2] Reworking specs

--- src/kirke/configurer.clj
+++ src/kirke/configurer.clj
@@ -5,30 +5,7 @@
 
 (defrecord Input
   [locations triggers])
-(spec/def ::url string?)
-(spec/def ::username string?)
-(spec/def ::password string?)
-(spec/def ::location
-  (spec/keys :req-un [::url ::username ::password]))
-(spec/def ::locations
-  (spec/every
-    (spec/or
-      :file-url-task string?
-      :database ::location)
-    :kind vector?))
-(spec/def ::async? boolean?)
-(spec/def ::trigger
-  (spec/keys :req-un [::async?]))
-(spec/def ::triggers
-  (spec/nilable
-    (spec/every
-      (spec/or
-        :string string?
-        :trigger ::trigger)
-      :kind vector?)))
-(spec/def ::input
-  (spec/keys :req-un [::locations ::triggers]))
-(spec/def ::output ::input)
+
 
 (defrecord Task
   [id input operation output])
--- src/kirke/spec/common.clj
+++ src/kirke/spec/common.clj
@@ -3,3 +3,12 @@
 
 (spec/def ::id string?)
 (spec/def ::has-id (spec/keys :req-un [::id]))
+(spec/def ::url string?)
+(spec/def ::username string?)
+(spec/def ::password string?)
+(spec/def ::location (spec/keys :req-un [::url] :opt-un [::username ::password]))
+(spec/def ::locations (spec/every (spec/or :file-url-task string? :database ::location) :kind vector?))
+(spec/def ::async? boolean?)
+(spec/def ::input (spec/keys :req-un [::locations]))
+(spec/def ::output ::input)
+(spec/def ::operation map?)
